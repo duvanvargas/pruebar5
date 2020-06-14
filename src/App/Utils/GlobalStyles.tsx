165d286ef65406a369b99b6b2bc6a61d0e8714f3
mac <mac@192.168.1.102>
2020-06-14 04:28:19 -0500

    fix import error

--- src/App/Utils/GlobalStyles.tsx
+++ src/App/Utils/GlobalStyles.tsx
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
   * {
     font-family: 'Lato', sans-serif;
     padding: 0px;
